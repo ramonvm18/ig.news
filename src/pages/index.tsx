fd5e74a62287f837651fc7494ff04c7ae8cec9d5
ramonvm18 <ramon.mastropaulo@hotmail.com>
2021-10-27 10:57:25 -0300

    SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
 import styles from './home.module.scss';
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import { stripe } from '../services/stripe';
 
 interface HomeProps {
@@ -34,7 +34,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
 
   const price = await stripe.prices.retrieve('price_1JmOu7BoVIL6bJHk6662bc3b')
 
@@ -49,8 +49,8 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product,
-
-    }
+    },
+    revalidate: 60 * 60 * 24, // 24 horas
   }
 
 }
